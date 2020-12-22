d2502513a9bdb0ef7184ad4c4188db3b1bdf228b
Brydon Johnson <brykeith@gmail.com>
2020-12-22 14:42:30 -0700

    changed projects to a list

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const menuIcon = document.querySelector('.hamburger-button');
-const navbar = document.querySelector('.menu-wrap');
+const menuIcon = document.querySelector(".hamburger-button");
+const navbar = document.querySelector(".menu-wrap");
+const projects = document.querySelectorAll(
+  ".projects-list-item-inner-container"
+);
 
 // adds and removes the "change" class from the navbar when you click on the hamburger-menu
-menuIcon.addEventListener('click', ()=> {navbar.classList.toggle("active");});
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+// clicking on project item
+projects.forEach((item) =>
+  item.addEventListener("click", () => {
+    item.classList.toggle("expand");
+  })
+);
